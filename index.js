b2ff8fabdd3e9ef98af9abc85076c94a602ccf69
EMON <mamudmdemon@gmail.com>
2024-05-14 05:22:13 +0600

    mongodb besic setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 
 const app=express()
@@ -10,6 +11,38 @@ const port=process.env.port || 5000;
 app.use(cors())
 app.use(express.json())
 
+
+
+
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.2fsgp3y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const serviceCollection=client.db("serviceDb").collection("service");
+
+
+    
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
